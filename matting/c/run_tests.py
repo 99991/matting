9a35b10e97284d8c096369147fc467951e20c4b0
matting <secret>
2019-02-26 11:04:16 +0100

    make tests reproducible by using our own rng

--- matting/c/run_tests.py
+++ matting/c/run_tests.py
@@ -1,15 +1,24 @@
 import os
+import sys
 
-err = os.system("gcc -Wall -Wextra -pedantic -lm tests.c -o tests")
+binary_path = "./tests"
 
-if err != 0:
+if sys.platform == "win32":
+    binary_path = "tests.exe"
+
+err = os.system("gcc -Wall -Wextra -pedantic -lm tests.c -o " + binary_path)
+
+if err == 0:
+    print("Tests compiled")
+else:
     print("Error: Failed to compile tests")
+    sys.exit(-1)
 
-err = os.system("./tests")
+err = os.system(binary_path)
 
-if err != 0:
+if err == 0:
+    print("All tests passed")
+else:
     print("Test failed")
 
-os.remove("./tests")
-
-print("tests passed")
+os.remove(binary_path)
--- matting/c/tests.c
+++ matting/c/tests.c
@@ -4,8 +4,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdint.h>
 
 #define SWAP(T, a, b) { T temp = (a); (a) = (b); (b) = temp; }
+
+uint32_t state = 0x12345678;
+
+uint32_t rand32(){
+    uint32_t x = state;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    state = x;
+    return x;
+}
 
 void naive_knn(
     float *data_points,
@@ -57,13 +69,13 @@ void naive_knn(
 }
 
 float randf(){
-    return rand() / (float)RAND_MAX;
+    return rand32() / (float)RAND_MAX;
 }
 
 void knn_test(){
     for (int point_dimension = 2; point_dimension <= 5; point_dimension++){
-        int n_data_points = rand() % 100;
-        int n_query_points = rand() % 100;
+        int n_data_points = rand32() % 100;
+        int n_query_points = rand32() % 100;
         
         float *data_points = (float*)malloc(n_data_points*point_dimension*sizeof(*data_points));
         float *query_points = (float*)malloc(n_query_points*point_dimension*sizeof(*query_points));
@@ -73,9 +85,9 @@ void knn_test(){
         }
         
         for (int i = 0; i < n_query_points; i++){
-            if (i > 0 && rand() % 100 == 0){
+            if (i > 0 && rand32() % 100 == 0){
                 // make duplicate point
-                int j = rand() % i;
+                int j = rand32() % i;
                 for (int k = 0; k < point_dimension; k++){
                     query_points[i*point_dimension + k] = query_points[j*point_dimension + k];
                 }
