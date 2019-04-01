b783149bdae57636cd65ef74e5f0029c5f3641fa
- <private@example.com>
2019-04-01 19:22:28 +0200

    commented knn_laplacian

--- matting/knn_laplacian.py
+++ matting/knn_laplacian.py
@@ -1,52 +1,61 @@
-from .util import solve_cg, make_system, weights_to_laplacian
+from .util import weights_to_laplacian
 from .knn import knn
 import numpy as np
 import scipy.sparse
 
-def knn_laplacian(image):
+def knn_laplacian(image, normalize=True):
+    # build knn laplacian from image using nearest neighbor information
     h,w,depth = image.shape
     n = h*w
     assert(depth == 3)
+    # calculate pixel positions
     x = np.arange(1, w + 1)
     y = np.arange(1, h + 1)
     x,y = np.meshgrid(x, y)
     x = x.flatten()
     y = y.flatten()
 
-    level = 2
-
-    d = np.sqrt(w*w + h*h)/level
-
+    # calculate feature matrix from colors and pixel positions
+    # distance scale factor
+    d = np.sqrt(w*w + h*h)/2
     r,g,b = image.reshape(n, 3).T
     features = np.stack([r,g,b,x/d,y/d], axis=1)
-    
     features = features.astype(np.float32)
-    
-    row = np.zeros((0, 2), dtype=np.int64)
+
+    # empty list of nearest neighbor pairs
+    ij = np.zeros((0, 2), dtype=np.int64)
 
     for n_neighbors in [10, 2]:
+        # for each pixel
         a = np.repeat(np.arange(n), n_neighbors)
+        # find nearest neighbor
         b = knn(features, features, n_neighbors).flatten()
 
-        more_rows = np.stack([
+        # sort nearest neighbor pairs by i-index
+        # (needed for uniqueness test latter)
+        more_ij = np.stack([
             np.minimum(a, b),
             np.maximum(a, b),
         ], axis=1)
 
-        row = np.concatenate([row, more_rows], axis=0)
+        # add new nearest neighbor pairs
+        ij = np.concatenate([ij, more_ij], axis=0)
         
         # decrease distance cost by scaling coordinates
         features[:, -2:] *= 0.01
 
-    row = np.unique(row, axis=0)
+    # discard duplicate nearest neighbor pairs
+    ij = np.unique(ij, axis=0)
 
-    i_inds = row[:, 0]
-    j_inds = row[:, 1]
+    i_inds = ij[:, 0]
+    j_inds = ij[:, 1]
 
+    # sophisticated weight function
     values = np.ones(len(i_inds))
 
+    # build weight matrix
     W = scipy.sparse.coo_matrix((values, (i_inds, j_inds)), shape=(n, n))
     
     W = W + W.T
     
-    return weights_to_laplacian(W)
+    return weights_to_laplacian(W, normalize=normalize)
