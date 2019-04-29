a9b0d3384a9a316ac23bd9dcbab4ff7324fe60a5
- <private@example.com>
2019-04-29 21:33:14 +0200

    faster handling of boundary conditions

--- matting/guided_filter.py
+++ matting/guided_filter.py
@@ -2,28 +2,34 @@ from .util import resize_nearest, resize_bilinear, vec_vec_outer
 from .boxfilter import boxfilter as _boxfilter
 import numpy as np
 
-def blur(image, radius):
-    return _boxfilter(image, radius, mode='same') / _boxfilter(np.ones_like(image), radius, mode='same')
+def blur_valid(image, radius):
+    return 1/(2*radius + 1)**2 * _boxfilter(image, radius, mode='valid')
 
-def _guided_filter(I, p, r, eps):
-    mean_I = blur(I, r)
-    mean_p = blur(p, r)
-    mean_Ip = blur(I * p[:, :, np.newaxis], r)
+def blur_full(image, radius):
+    return 1/(2*radius + 1)**2 * _boxfilter(image, radius, mode='full')
+
+def local_model(I, p, r, eps):
+    mean_I = blur_valid(I, r)
+    mean_p = blur_valid(p, r)
+    mean_pI = blur_valid(p[:, :, np.newaxis] * I, r)
     
-    cov_Ip = mean_Ip - mean_I * mean_p[:, :, np.newaxis]
-    var_I = blur(vec_vec_outer(I, I), r) - vec_vec_outer(mean_I, mean_I)
+    v = mean_pI - mean_I * mean_p[:, :, np.newaxis]
     
-    a = np.linalg.solve(var_I + eps*np.eye(3), cov_Ip)
+    covariance = blur_valid(vec_vec_outer(I, I), r) - vec_vec_outer(mean_I, mean_I)
+    
+    a = np.linalg.solve(covariance + eps*np.eye(3), v)
     b = mean_p - (a * mean_I).sum(axis=2)
 
     return a, b
 
-def guided_filter(I, p, r, eps):
-    a, b = _guided_filter(I, p, r, eps)
+def guided_filter(I, p, r, eps=1e-4):
+    a, b = local_model(I, p, r, eps)
     
-    return (blur(a, r)*I).sum(axis=2) + blur(b, r)
+    c = blur_full(np.ones_like(b), r)
+    
+    return ((blur_full(a, r)*I).sum(axis=2) + blur_full(b, r))/c
 
-def fast_guided_filter(I0, p0, r0, eps, scale=0.125):
+def fast_guided_filter(I0, p0, r0, eps=1e-4, scale=0.125):
     h0,w0 = I0.shape[:2]
     
     r = int(r0*scale)
@@ -33,9 +39,11 @@ def fast_guided_filter(I0, p0, r0, eps, scale=0.125):
     I = resize_nearest(I0, w, h)
     p = resize_nearest(p0, w, h)
     
-    a,b = _guided_filter(I, p, r, eps)
+    a,b = local_model(I, p, r, eps)
     
-    a = resize_bilinear(a, w0, h0)
-    b = resize_bilinear(b, w0, h0)
+    a = resize_bilinear(a, w0 - 2*r0, h0 - 2*r0)
+    b = resize_bilinear(b, w0 - 2*r0, h0 - 2*r0)
 
-    return (blur(a, r)*I0).sum(axis=2) + blur(b, r)
+    c = blur_full(np.ones_like(b), r0)
+    
+    return ((blur_full(a, r0)*I0).sum(axis=2) + blur_full(b, r0))/c
